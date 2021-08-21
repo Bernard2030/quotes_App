bbfc19b41a69257a6028d4d27b705a488e7fca57
Bernard2030 <bernard.mukhenye@student.moringa.com>
2021-08-21 15:03:31 +0300

    add constructor

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,14 +8,12 @@ import { Quotes } from '../quotes';
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[] = [
-    new Quotes(1, "Morgan", "confidence", "No body can make you feel inferior without your consent", new Date("2021, 5, 16"))
-    new Quotes(1, "BenCarson", )
-    // {1, 'Morgan', 'Confidence', 'No body can make you feel inferior without your consent'},
-    // {id:2, :'Bencarson', quoteType:'Inspiration', description:'Aperson who never made a mistake never tried anything new.'},
-    // {id:3, name:'Lawrence', quoteType:'Love', description:'Love your neigbour as you love your self'},
-    // {id:4, name:'Sydney', quoteType:'Success', description:'Continuous efforts, not strength or intelligence is the key to unlocking our potential'},
-    // {id:5, name:'Dorcas', quoteType:'Love', description:'No woman ever hates a man for being in love with her,but many a woman hates a man for being a friend to her'},
-    // {id:6, name:'Bernard', quoteType:'Change', description:'They say that time changes things but you actually have to change them yourself'},
+    new Quotes(1, "Morgan", "confidence", "No body can make you feel inferior without your consent", new Date("2021, 5, 16")),
+    new Quotes(2, "BenCarson","Inspiration", "A person who never made a mistake never tried anything new", new Date("2016, 2, 12")),
+    new Quotes(3,"Lawrence", "Love", "Love your neigbour as you love your self", new Date("2018,9,9")),
+    new Quotes(4, "Dorcas", "Love", "No woman ever hates a man for being in love with her,but many a woman hates a man for being a friend to her", new Date("2021, 6, 1")),
+    new Quotes(5, "Bernard", "Change", "They say that time changes things but you actually have to change them yourself", new Date("2021, 8, 10"))
+   
   ];
 
   constructor() { }
